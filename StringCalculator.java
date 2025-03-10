89639666d56b5bc598a8fc4916e2d27f39098c8b
Sukhada <31947638+SukhadaKS@users.noreply.github.com>
2025-03-10 09:41:52 +0530

    empty string or single digit number

---
+++ StringCalculator.java
@@ -0,0 +1,36 @@
+package com.tdd;
+
+public class StringCalculator_1 {
+
+    public int add(String numbers) {
+        int sum = 0;
+        if (numbers.isEmpty()) {
+            return 0;
+        }
+        else if (numbers.length() == 2) {
+            sum = Integer.parseInt(numbers);
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        StringCalculator_1 stringCalculator = new StringCalculator_1();
+
+        String result = (stringCalculator.add("") == 0) ? "success.." : "failure..";
+        System.out.println(result);
+
+        result = (stringCalculator.add("1") == 1) ? "success.." : "failure..";
+        System.out.println(result);
+
+        result = (stringCalculator.add("3") == 3) ? "success.." : "failure..";
+        System.out.println(result);
+
+        result = (stringCalculator.add("9") == 3) ? "success.." : "failure..";
+        System.out.println(result);
+
+        stringCalculator = null;
+    }
+
+
+}
