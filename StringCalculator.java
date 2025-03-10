cb7609569140af119099f3afb1733378eb377b4d
Sukhada <31947638+SukhadaKS@users.noreply.github.com>
2025-03-10 09:58:34 +0530

    comma as delimiter

--- StringCalculator.java
+++ StringCalculator.java
@@ -1,21 +1,26 @@
 package com.tdd;
 
-public class StringCalculator {
+public class StringCalculator_1 {
 
     public int add(String numbers) {
-        int sum = 0;
+        int total = 0;
         if (numbers.isEmpty()) {
             return 0;
         }
-        else if (numbers.length() == 2) {
-            sum = Integer.parseInt(numbers);
+        else if (numbers.length() == 1) {
+            total = Integer.parseInt(numbers);
+        }
+        else {
+            for(String num: numbers.split(",")){
+                total += Integer.parseInt(num);
+            }
         }
 
-        return sum;
+        return total;
     }
 
     public static void main(String[] args) {
-        StringCalculator stringCalculator = new StringCalculator();
+        StringCalculator_1 stringCalculator = new StringCalculator_1();
 
         String result = (stringCalculator.add("") == 0) ? "success.." : "failure..";
         System.out.println(result);
@@ -23,10 +28,10 @@ public class StringCalculator {
         result = (stringCalculator.add("1") == 1) ? "success.." : "failure..";
         System.out.println(result);
 
-        result = (stringCalculator.add("3") == 3) ? "success.." : "failure..";
+        result = (stringCalculator.add("3,5") == 8) ? "success.." : "failure..";
         System.out.println(result);
 
-        result = (stringCalculator.add("9") == 3) ? "success.." : "failure..";
+        result = (stringCalculator.add("9,1") == 10) ? "success.." : "failure..";
         System.out.println(result);
 
         stringCalculator = null;
